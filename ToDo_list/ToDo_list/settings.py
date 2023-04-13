a15da8c8cca95e48890ede61cbbef47f3fff8ddc
mitatskni <mitatskni@gmail.com>
2023-04-13 15:55:06 +0200

    nothing

--- ToDo_list/ToDo_list/settings.py
+++ ToDo_list/ToDo_list/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from ToDo_list import local_settings
+import local_settings
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
 ]
 
 MIDDLEWARE = [
